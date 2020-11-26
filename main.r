6bfa86e1ddcc20304f90ec845284a2b9bd3e613e
4nar <anar.abetayeva@gmail.com>
2020-11-26 16:09:53 +0600

    changes in main

--- main.r
+++ main.r
@@ -8,4 +8,4 @@ df <- read.csv('ligue1-20192020-results.csv')
 stan_data <- list(N=nrow(df), nteams=length(unique(df$home.team)), y1=df$h.g., y2=df$h.a., hometeam=df$home.team, awayteam=df$away.team)
 sm <- rstan::stan_model(file = "model_init.stan")
 
-model <- rstan::sampling(sm, data = stan_data, control = list(adapt_delta = 0.99, max_treedepth = 15))
+model <- rstan::sampling(sm, data = stan_data, control = list(adapt_delta = 0.99, max_treedepth = 11))
